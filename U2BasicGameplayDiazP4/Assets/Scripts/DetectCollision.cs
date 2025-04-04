9f0bae084219a26812b83fe6328be5605bf50cd0
Bitt3rcrawlANA <722697@student.dusd.net>
2025-04-04 11:47:04 -0700

    aw3e45yuhw345yu
    456uji64iuj534u6537uy75yu3

--- U2BasicGameplayDiazP4/Assets/Scripts/DetectCollision.cs
+++ U2BasicGameplayDiazP4/Assets/Scripts/DetectCollision.cs
@@ -18,14 +18,14 @@ public class DetectCollision : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player"))                                
         {
             gameManager.AddLives(-1);
             Destroy(gameObject);
         }
-        else if (other.CompareTag("Sandwich"))
+        else if (other.CompareTag("Animal"))
         {
             other.GetComponent<AnimalHunger>().FeedAnimal(1);
         }
